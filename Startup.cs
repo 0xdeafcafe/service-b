d88f8bb9e6e13bc6517fb1f7d8cb20d154f6f073
Alex Forbes-Reed <alexreed@outlook.com>
2016-08-10 14:31:08 +0100

    Initial Commit

---
+++ Startup.cs
@@ -0,0 +1,52 @@
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+
+namespace ServiceB
+{
+	public class Startup
+	{
+		public Startup(IHostingEnvironment env)
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(env.ContentRootPath)
+				.AddJsonFile("config.json", optional: true, reloadOnChange: true)
+				.AddJsonFile($"config.{env.EnvironmentName}.json", optional: true)
+				.AddEnvironmentVariables();
+			Configuration = builder.Build();
+		}
+
+		public IConfigurationRoot Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			// Add framework services.
+			services.AddMvc();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+			loggerFactory.AddDebug();
+
+			app.UseMvc();
+			app.Map("/", builder =>
+			{
+				builder.Run(async ctx =>
+				{
+					ctx.Response.Headers.Add("Content-Type", new StringValues("application/json"));
+					ctx.Response.Body = new MemoryStream(Encoding.UTF8.GetBytes("{ \"alive\": true }"));
+					await Task.Delay(1);
+				});
+			});
+		}
+	}
+}
